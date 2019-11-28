2b6e842fa8204a3363c8c37e9a0c59278364d519
Jesús Pérez <jjchiva94@hotmail.com>
2019-11-28 18:10:59 +0100

    añadido modelo jugadores

---
+++ src/main/java/com/geekshubs/manager_deportivo/models/Jugador.java
@@ -0,0 +1,12 @@
+package com.geekshubs.manager_deportivo.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jugador")
+public class Jugador {
+
+
+}
